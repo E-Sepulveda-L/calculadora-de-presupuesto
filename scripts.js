c76bbcdd979a5996e330b2b2e1d10434d7538487
erick <mate.sepuls@gmail.com>
2025-02-03 19:38:19 -0500

    operaciones

--- scripts.js
+++ scripts.js
@@ -1,26 +1,36 @@
-const incomes = document.querySelectorAll('.incomes-input');
-const totalIncomesSpan = document.getElementById('total-incomes-span');
+const incomesInput = document.querySelectorAll('.incomes-input');
+const resumeIncomesSpan = document.getElementById('resume-incomes-span');
+const resumeBalanceSpan = document.getElementById('resume-balance-span');
 
 function sumOfValues (inputs, result){
     return function () {
         let sum = 0;
         inputs.forEach(num => {
             if(num.value.trim() !== ''){
-                sum += Number(num.value);
+                sum += Number(num.value)
             }
         });
         result.textContent = sum;
+        updateBalance()
     };
 }
 
-incomes.forEach(input => {
-    input.addEventListener('input', sumOfValues(incomes, totalIncomesSpan))
+incomesInput.forEach(input => {
+    input.addEventListener('input', sumOfValues(incomesInput, resumeIncomesSpan))
 })
 
 const expensesInput = document.querySelectorAll('.expenses-input');
+const resumeExpensesSpan = document.getElementById('resume-expenses-span');
 
+expensesInput.forEach(input => {
+    input.addEventListener('input', sumOfValues(expensesInput, resumeExpensesSpan))
+})
 
-
+function updateBalance() {
+    const totalIncome = resumeIncomesSpan.textContent
+    const totalExpenses = resumeExpensesSpan.textContent
+    resumeBalanceSpan.textContent = totalIncome - totalExpenses
+}
 
 
 
